ed1a007ed49b8322dda7c050507c2379b97b7889
StarClient <klyleenb@gmail.com>
2025-09-20 10:51:04 -0300

    Add files via upload

---
+++ app.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+import { Analytics } from '@vercel/analytics/nuxt'
+</script>
+<template>
+  <SpeedInsights/>
+  <Analytics/>
+  <div class="min-h-screen">
+    <NavBar />
+    <NuxtPage />
+    <Footer />
+  </div>
+</template>
+<style>
+@import url('assets/css/tailwind.css');
+</style>
